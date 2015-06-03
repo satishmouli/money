94bb538b01afd46e0866be3d92229938d12d585c
manasUL <manas.galipalli@urbanladder.com>
2015-06-03 15:06:37 +0530

    Adding + operator to Money

--- lib/indian_currency.rb
+++ lib/indian_currency.rb
@@ -2,16 +2,20 @@
 class IndianCurrency
   attr_reader :rupee, :paise
   def initialize(rupee, paise)
-    @rupee = rupee
-    @paise = paise
+    @rupee = rupee + paise/100
+    @paise = paise % 100
   end
 
   def ==(other)
-    (@rupee == other.rupee && @paise == other.paise)
+    if other && other.class == self.class
+      (@rupee == other.rupee && @paise == other.paise)
+    end
+  end
+  
+  def +(other)
+    IndianCurrency.new(@rupee + other.rupee, @paise + other.paise)
   end
 
-  def to_s
-    "5.40"
-  end
+  #[rupee, paise].hash
   alias_method :eql?, :==
 end
--- spec/indian_currency_spec.rb
+++ spec/indian_currency_spec.rb
@@ -18,8 +18,28 @@ describe IndianCurrency do
     expect(indian_currency1).to_not eq(indian_currency2)
   end
 
-  it "money with 5 rupee and 40 paise should return a string 5.40" do
-    indian_currency = IndianCurrency.new(5,40)
-    expect(indian_currency.to_s).to eq("5.40")
+  it "money with 1 rupee and 40 paise should be equal to money with 0 rupee and 140 paise" do
+    indian_currency1 = IndianCurrency.new(1,40)
+    indian_currency2 = IndianCurrency.new(0,140)
+    expect(indian_currency1).to eq(indian_currency2)
+  end
+
+  it "money is not equal to nil money object" do
+    indian_currency1 = IndianCurrency.new(10,40)
+    indian_currency2 = nil
+    expect(indian_currency1).to_not eq(indian_currency2)
+  end
+
+  it "money is not equal to some object" do
+    indian_currency1 = IndianCurrency.new(10,40)
+    indian_currency2 = 5
+    expect(indian_currency1).to_not eq(indian_currency2)
+  end
+
+  it "sum of money with 5 rupees 40 paise and 1 rupee 30 paise should be equal to money with 6 rupees 70 paise" do
+    indian_currency1 = IndianCurrency.new(5,40)
+    indian_currency2 = IndianCurrency.new(1,30)
+    indian_currency3 = IndianCurrency.new(6,70)
+    expect(indian_currency1 + indian_currency2).to eq(indian_currency3)
   end
 end
